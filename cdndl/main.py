92cfcbe671fac2dd0d00ffa5b8db9e9583939374
co-co <co-co@no-reply.com>
2024-11-21 14:05:27 +0800

    支持重定向

--- cdndl/main.py
+++ cdndl/main.py
@@ -4,7 +4,6 @@
 import argparse
 from os import path
 import random
-import re
 from tqdm import tqdm
 import urllib3
 import ipaddress
@@ -48,40 +47,61 @@ def download_file(url: str, save_path: str, cdn_config: str, ua: bool, ts: int):
     hostname, path = parse_url(url)
     ip, port = parse_cdn_config(cdn_config)
     pool = urllib3.HTTPSConnectionPool(
-            ip,
-            assert_hostname=hostname,
-            server_hostname=hostname,
-            port=port,
-            cert_reqs='CERT_NONE')
+        ip,
+        assert_hostname=hostname,
+        server_hostname=hostname,
+        port=port,
+        cert_reqs='CERT_NONE',
+    )
     headers = {
         'Host': hostname,
     }
     if ua:
         headers.update({'User-Agent': random.choice(USER_AGENTS)})
-    try:
-        response = pool.urlopen('GET', path,
-                         redirect=True,
-                         headers=headers,
-                         assert_same_host=False,
-                         timeout=15,
-                         preload_content=False,
-                         retries=urllib3.util.Retry(10, backoff_factor=1))
-        total_size = int(response.headers.get('content-length', 0))
-        with open(save_path, 'wb') as file, tqdm(
-            desc=save_path,
-            total=total_size,
-            unit='K',
-            unit_scale=True,
-            unit_divisor=1024,
-        ) as bar:
-            for data in response.stream(ts):
-                # 写入文件并更新进度条
-                file.write(data)
-                bar.update(len(data))
-    except Exception as e:
-        print('下载文件异常:', e)
-        return False
-    return True
+    while True:
+        try:
+            response = pool.urlopen('GET',path,
+                             redirect=False,
+                             headers=headers,
+                             assert_same_host=False,
+                             timeout=10,
+                             preload_content=False,
+                             retries=urllib3.util.Retry(10, backoff_factor=1))
+            # 检查是否为重定向
+            if response.status in (301, 302, 303, 307, 308) and 'Location' in response.headers:
+                # 获取重定向的 URL
+                redirect_url = response.headers["Location"]
+                hostname, path = parse_url(redirect_url)
+                # 为新路径建立新的连接池，将 hostname 指向目标 IP 和端口
+                pool = urllib3.HTTPSConnectionPool(
+                    ip,
+                    assert_hostname=hostname,
+                    server_hostname=hostname,
+                    port=port,
+                    cert_reqs='CERT_NONE',
+                )
+                headers = response.headers
+                headers.update(
+                    {'Host': hostname}
+                )
+                continue
+            total_size = int(response.headers.get('content-length', 0))
+            with open(save_path, 'wb') as file, tqdm(
+                desc=save_path,
+                total=total_size,
+                unit='K',
+                unit_scale=True,
+                unit_divisor=1024,
+            ) as bar:
+                for data in response.stream(ts):
+                    # 写入文件并更新进度条
+                    file.write(data)
+                    bar.update(len(data))
+                response.release_conn()
+                return True
+        except Exception as e:
+            print('下载文件异常:', e)
+            return False
 
 def main():
     parser = argparse.ArgumentParser(description='cdn-dl 下载配置')
