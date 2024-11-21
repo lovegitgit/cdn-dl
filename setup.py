9f2b5e99077a07628c020ef245ffa8ee0e13f873
co-co <co-co@no-reply.com>
2024-11-21 11:23:13 +0800

    init

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+import os
+
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='cdn-dl',
+    version='0.1',
+    description='nftset updater for openwrt',
+    long_description=long_description,
+    author="nobitaqaq",
+    author_email="xiaoleigs@gmail.com",
+    keywords=["cdn-dl", "cdn 文件下载器"],
+    packages=find_packages(include=['cdndl']),
+    entry_points={
+        'console_scripts': [
+            'cdn-dl = cdndl.main:main',
+        ]
+    },
+    python_requires=">=3.8",
+    install_requires=[
+        'tqdm',
+        'urllib3'
+    ],
+)
