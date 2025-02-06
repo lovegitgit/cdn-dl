18115e416d1e1bf86fc634062b11de22a6b1228c
co-co <co-co@no-reply.com>
2025-02-06 16:33:38 +0800

    refine code

--- cdndl/main.py
+++ cdndl/main.py
@@ -8,8 +8,10 @@ import os
 import random
 import re
 import signal
+from time import sleep
 from typing import List
 from concurrent.futures import ThreadPoolExecutor, as_completed
+import requests
 from tqdm import tqdm
 import urllib3
 import ipaddress
@@ -18,6 +20,10 @@ from collections import defaultdict
 from urllib3.exceptions import ConnectTimeoutError, MaxRetryError
 
 DEBUG = False
+# 用这个key 表明可以匹配任意域名
+ALL_DOMAIN_KEY = 'all_domain'
+# 默认ALI DNS API
+DEF_DNS_API = 'https://dns.alidns.com/resolve?name={}&type=1'
 
 USER_AGENTS = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
@@ -46,7 +52,7 @@ def is_ip_address(ip_str: str):
 
 HOSTNAME_PATTERN = r'^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$'
 def is_domain_name(string: str):
-    if re.match(HOSTNAME_PATTERN, string):
+    if re.match(HOSTNAME_PATTERN, string) or string == ALL_DOMAIN_KEY:
         return True
     else:
         return False
@@ -54,44 +60,47 @@ def is_domain_name(string: str):
 def is_valid_port(port: int):
     return port > 0 and port < 65535
 
-def parse_cdn_config(hostname: str, cdn_configs: str):
-    def parse_str_config(config_str: str, hostname: str):
-        def method1(hostname):
-            parts = config_str.split()
-            ip = parts[0]
-            hostname = parts[1] if len(parts) == 2 else hostname
-            port = 443
-            return hostname, ip, port
 
-        def method2(hostname):
-            parts = config_str.split(':')
-            ip = parts[0]
-            port = 443
-            if len(parts) >= 2:
-                if is_domain_name(parts[1]):
-                    hostname = parts[1]
-                else:
-                    try:
-                        port = int(parts[1])
-                    except:
-                        port =  -1
-            if len(parts) >= 3:
+def parse_str_config(config_str: str):
+    def method1():
+        parts = config_str.split()
+        ip = parts[0]
+        hostname = parts[1] if len(parts) == 2 else ALL_DOMAIN_KEY
+        port = 443
+        return hostname, ip, port
+
+    def method2():
+        parts = config_str.split(':')
+        ip = parts[0]
+        port = 443
+        hostname = ALL_DOMAIN_KEY
+        if len(parts) >= 2:
+            if is_domain_name(parts[1]):
+                hostname = parts[1]
+            else:
                 try:
                     port = int(parts[1])
                 except:
-                    port = -1
-                hostname = parts[2]
+                    port =  -1
+        if len(parts) >= 3:
+            try:
+                port = int(parts[1])
+            except:
+                port = -1
+            hostname = parts[2]
+        return hostname, ip, port
+
+    for fn in method1, method2:
+        hostname, ip, port = fn()
+        if is_domain_name(hostname) and is_ip_address(ip) and is_valid_port(port):
             return hostname, ip, port
+    return None, None, None
 
-        for fn in method1, method2:
-            hostname, ip, port = fn(hostname)
-            if is_domain_name(hostname) and is_ip_address(ip) and is_valid_port(port):
-                return hostname, ip, port
-        return None, None, None
 
-    hostname_ip_map = defaultdict(list)
-    config_lines = []
-    for cdn_config in cdn_configs:
+def gen_cdn_map(configs: List[str]):
+    cdn_map = defaultdict(list)
+    real_configs = []
+    for cdn_config in configs:
         file_path = path.join(cdn_config)
         if path.exists(file_path) and path.isfile(file_path):
             with open(file_path, 'r', encoding='utf-8') as f:
@@ -99,44 +108,75 @@ def parse_cdn_config(hostname: str, cdn_configs: str):
                     line = line.strip()
                     if not line or line.startswith("#"):
                         continue
-                    config_lines.append(line)
+                    real_configs.append(line)
         else:
-            config_lines.append(cdn_config)
-
-    for config_str in config_lines:
-        hostname, ip, port = parse_str_config(config_str, hostname)
-        if hostname:
-            hostname_ip_map[hostname].append((ip, port))
-    for k, v in hostname_ip_map.items():
-        hostname_ip_map[k] = list(dict.fromkeys(v))
-    if DEBUG and hostname_ip_map:
+            real_configs.append(cdn_config)
+    for cfg in real_configs:
+        hostname, ip, port = parse_str_config(cfg)
+        cdn_map[hostname].append((ip, port))
+    for k, v in cdn_map.items():
+        cdn_map[k] = list(dict.fromkeys(v))
+    if DEBUG and cdn_map:
         print()
         print('CDN 配置如下:')
-        for k, v in hostname_ip_map.items():
+        for k, v in cdn_map.items():
             print('    ',k, v)
         print()
-    return hostname_ip_map
+    return cdn_map
 
+class Cdn:
+    def __init__(self, configs: List[str]):
+        self.cdn_map = gen_cdn_map(configs)
+        self.choice_map = defaultdict(list)
+
+    def get_all_cdn_for_domain(self, hostname):
+        choices = self.choice_map.get(hostname)
+        if not choices:
+            choices = self.cdn_map.get(hostname, [])
+            all_domain_choices = self.cdn_map.get(ALL_DOMAIN_KEY, [])
+            choices.extend(all_domain_choices)
+            if not choices:
+                dns = dns_lookup(hostname)
+                for r in dns:
+                    choices.append((r, 443))
+                    self.cdn_map[hostname].append((r, 443))
+            choices = list(dict.fromkeys(choices))
+            self.choice_map[hostname] = choices
+        return choices
 
 def parse_url(url: str):
     parsed_url = urlparse(url)
     return parsed_url.hostname, parsed_url.path
 
-def download_file(url: str, save_path: str, cdn_configs: List[str], ua: bool, ts: int, timeout: int, retry: int):
-    def init_cdn_map():
-        hostname, _ = parse_url(url)
-        cdn_map = parse_cdn_config(hostname, cdn_configs)
-        if not cdn_map:
-            raise RuntimeError('无法检测到可用CDN, 请检查配置!')
-        return cdn_map
+def dns_lookup(domain):
+    def dns_lookup_internal():
+        has_error = False
+        dns = []
+        try:
+            res = requests.get(DEF_DNS_API.format(domain), headers={'accept': 'application/dns-json'})
+            json_data = res.json()
+            if 'Answer' in json_data:
+                records = json_data['Answer']
+                for record in records:
+                    if record['type'] == 1:
+                        dns.append(record['data'])
+        except:
+            has_error = True
+        return has_error, dns
 
-    def get_ip_port(hostname: str):
-        choices = cdn_map.get(hostname)
-        if not choices:
-            choices = []
-            for _, v in cdn_map.items():
-                choices.extend(v)
-        used_choices = bad_cdn_map.get(hostname, [])
+    print(f"Performing DNS lookup for {domain}...")
+    has_error, dns = dns_lookup_internal()
+    retry_cnt = 0
+    while has_error and retry_cnt < 3:
+        stay_seconds = round(random.uniform(1, 3), 2)
+        print(f"Retrying DNS lookup for {domain} in {stay_seconds}s...")
+        sleep(stay_seconds)
+        has_error, dns = dns_lookup_internal()
+        retry_cnt += 1
+    return dns
+
+def download_file(url: str, save_path: str, cdn_configs: List[str], ua: bool, ts: int, timeout: int, retry: int):
+    def get_ip_port(choices: list, used_choices: list):
         available_choices = [choice for choice in choices if choice not in used_choices]
         if available_choices:
             return random.choice(available_choices)
@@ -144,7 +184,7 @@ def download_file(url: str, save_path: str, cdn_configs: List[str], ua: bool, ts
             return None, None
 
     res = False
-    cdn_map = init_cdn_map()
+    cdn = Cdn(cdn_configs)
     headers = {}
     if ua:
         headers.update({'User-Agent': random.choice(USER_AGENTS)})
@@ -155,7 +195,7 @@ def download_file(url: str, save_path: str, cdn_configs: List[str], ua: bool, ts
             bad_cdn_map[hostname].append((ip, port))
 
         hostname, _ = parse_url(url)
-        ip, port = get_ip_port(hostname)
+        ip, port = get_ip_port(cdn.get_all_cdn_for_domain(hostname), bad_cdn_map.get(hostname, []))
         if not ip:
             print('所有CDN 都无法下载, 退出中... ...')
             break
@@ -209,6 +249,7 @@ def download_file(url: str, save_path: str, cdn_configs: List[str], ua: bool, ts
             break
     return res
 
+
 def get_cdn():
     def parse_domains():
         domains = []
@@ -230,18 +271,20 @@ def get_cdn():
         return domains
 
     def init_cdn_map():
-        hostname, _ = parse_url(url)
-        cdn_map = parse_cdn_config(hostname, cdn_configs)
+        cdn_map = gen_cdn_map(cdn_configs)
         return cdn_map
 
-    def get_ip_port(hostname: str, used_choices: list):
+    def gen_all_choices_for_domain(hostname):
         if not cdn_map:
-            return None, None
+            return []
         choices = cdn_map.get(hostname)
         if not choices:
             choices = []
             for _, v in cdn_map.items():
                 choices.extend(v)
+        return choices
+
+    def get_ip_port(choices: list, used_choices: list):
         available_choices = [choice for choice in choices if choice not in used_choices]
         if available_choices:
             return random.choice(available_choices)
@@ -258,7 +301,7 @@ def get_cdn():
                 hostname, _ = parse_url(url)
                 headers.update({'Host': hostname})
                 while True:
-                    if not cdn_map:
+                    if not choices:
                         print('CDN 为空, 跳过使用CDN 解析')
                         pool = urllib3.HTTPSConnectionPool(
                             hostname,
@@ -267,7 +310,7 @@ def get_cdn():
                             cert_reqs='CERT_NONE',
                         )
                     else:
-                        ip, port = get_ip_port(hostname, used_choices)
+                        ip, port = get_ip_port(choices, used_choices)
                         if not ip:
                             print('所有CDN 都无法解析, 退出中... ...')
                             break
@@ -304,6 +347,7 @@ def get_cdn():
                 return dns
 
             print(f"Performing DNS lookup for {domain}...")
+            choices = gen_all_choices_for_domain(domain)
             dns = dns_lookup_internal()
             return domain, dns
 
@@ -346,7 +390,7 @@ def get_cdn():
     parser.add_argument('-r', '--retry', type=int, default=3, help='下载请求重试次数, 默认3')
     parser.add_argument('domain', nargs='+', help='需要获取cdn的域名或者文本')
     # 'https://dns.google/resolve?name={}&type=A'
-    parser.add_argument('--api', type=str, default='https://dns.alidns.com/resolve?name={}&type=1', help='dns api, 默认ali, 使用CF 使用cf dns')
+    parser.add_argument('--api', type=str, default=DEF_DNS_API, help='dns api, 默认ali, 使用CF 使用cf dns')
     parser.add_argument('-d', '--debug', action='store_true', default=False, help="是否打印调试信息")
     args = parser.parse_args()
     domain_arg = args.domain
